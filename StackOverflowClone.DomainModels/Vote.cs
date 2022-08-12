da15a5d52fb8e587f94d075e5e12b237295fc30e
Julian Radevych <jradevych@munisight.com>
2022-08-12 16:20:16 -0600

    <Build: Domain Models created>

---
+++ StackOverflowClone.DomainModels/Vote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace StackOverflowClone.DomainModels
+{
+    public class Vote
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VoteID { get; set; }
+        public int UserID { get; set; }
+        public int AnswerID { get; set; }
+        public int VoteValue { get; set; }
+
+
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+        [ForeignKey("AnswerID")]
+        public virtual Answer Answer { get; set; }
+    }
+}
