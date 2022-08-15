6fcc3c74fc09010485ecd55db8e1045472746a9a
Julian Radevych <jradevych@munisight.com>
2022-08-15 16:59:32 -0600

    <Build: categories repository completed>

---
+++ StockOverflowClone.Repositories/CategoriesRepositories.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StackOverflowClone.DomainModels;
+
+namespace StockOverflowClone.Repositories
+{
+    public interface ICategoriesrepositories
+    {
+        void InsertCategory(Category cat);
+        void UpdateCategory(Category cat);
+        void DeleteCategory(Category cat);
+        List<Category> GetAllCategories();
+        List<Category> GetCategoryByCategoryID(int CategoryID);
+
+    }
+
+    public class CategoriesRepositories : ICategoriesrepositories
+    {
+
+        StackOverflowDbContext db;
+
+        public CategoriesRepositories()
+        {
+            db = new StackOverflowDbContext();
+        }
+
+        public void DeleteCategory(Category cat)
+        {
+            Category existingCategory = db.Categories.Where(temp => temp.CategoryID == cat.CategoryID).FirstOrDefault();
+            if (existingCategory != null)
+            {
+                db.Categories.Remove(existingCategory);
+
+                db.SaveChanges();
+            }
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return categories;
+        }
+
+        public List<Category> GetCategoryByCategoryID(int CategoryID)
+        {
+            List<Category> categoriesByID = db.Categories.Where(temp => temp.CategoryID == CategoryID).ToList();
+            return categoriesByID;
+        }
+
+        public void InsertCategory(Category cat)
+        {
+            db.Categories.Add(cat);
+            db.SaveChanges();
+        }
+
+        public void UpdateCategory(Category cat)
+        {
+            Category existingCategory = db.Categories.Where(temp => temp.CategoryID == cat.CategoryID).FirstOrDefault();
+            if (existingCategory != null)
+            {
+
+                existingCategory.CategoryName = cat.CategoryName;
+                db.SaveChanges();
+            }
+        }
+    }
+}
