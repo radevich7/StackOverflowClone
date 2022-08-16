942052060bd573610a8deaff008c7fd23b407c00
Julian Radevych <jradevych@munisight.com>
2022-08-15 18:42:13 -0600

    <Build: NewquestionViewModel and EditUserPasswordViewModel>

---
+++ StackOverflowClone.ViewModels/EdituserPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace StackOverflowClone.ViewModels
+{
+    public class EdituserPasswordViewModel
+    {
+        [Required]
+        public int UserID { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
