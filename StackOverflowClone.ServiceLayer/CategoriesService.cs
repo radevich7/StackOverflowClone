4afef8a096198851e5e52a17a215440caad33b77
Julian Radevych <jradevych@munisight.com>
2022-08-17 17:59:48 -0600

    <Build: Question Service completed>

---
+++ StackOverflowClone.ServiceLayer/CategoriesService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowClone.DomainModels;
+using StackOverflowClone.ViewModels;
+using StackOverflowClone.Repositories;
+
+using AutoMapper;
+using AutoMapper.Configuration;
+
+
+namespace StackOverflowClone.ServiceLayer
+{
+    public interface ICategoriesService
+    {
+        void InsertCategory(CategoriesViewModel categoriesViewModel);
+        void UpdateCategory(CategoriesViewModel categoriesViewModel);
+        void DeleteCategory(CategoriesViewModel categoriesViewModel);
+        List<CategoriesViewModel> GetAllCategories();
+        CategoriesViewModel GetCategoryByCategoryID(int CategoryID);
+
+    }
+    public class CategoriesService : ICategoriesService
+    {
+        ICategoriesrepositories cr;
+        public CategoriesService()
+        {
+            cr = new CategoriesRepository();
+        }
+
+        public void DeleteCategory(CategoriesViewModel categoriesViewModel)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<CategoriesViewModel, Category>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Category cat = mapper.Map<CategoriesViewModel, Category>(categoriesViewModel);
+            cr.DeleteCategory(cat);
+        }
+
+        public List<CategoriesViewModel> GetAllCategories()
+        {
+
+            List<Category> categoriesRep = cr.GetAllCategories();
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Category, CategoriesViewModel>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            List<CategoriesViewModel> categoriesSer = mapper.Map<List<Category>, List<CategoriesViewModel>>(categoriesRep);
+            return categoriesSer;
+
+        }
+
+        public CategoriesViewModel GetCategoryByCategoryID(int CategoryID)
+        {
+            Category existingCategory = cr.GetCategoryByCategoryID(CategoryID).FirstOrDefault();
+            CategoriesViewModel category = null;
+            if (existingCategory != null)
+            {
+                var config = new MapperConfiguration(cfg => { cfg.CreateMap<Category, CategoriesViewModel>(); cfg.IgnoreUnmapped(); });
+                IMapper mapper = config.CreateMapper();
+                category = mapper.Map<Category, CategoriesViewModel>(existingCategory);
+            }
+            return category;
+        }
+
+        public void InsertCategory(CategoriesViewModel categoriesViewModel)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<CategoriesViewModel, Category>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Category cat = mapper.Map<CategoriesViewModel, Category>(categoriesViewModel);
+            cr.InsertCategory(cat);
+        }
+
+        public void UpdateCategory(CategoriesViewModel categoriesViewModel)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<CategoriesViewModel, Category>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Category cat = mapper.Map<CategoriesViewModel, Category>(categoriesViewModel);
+            cr.UpdateCategory(cat);
+        }
+    }
+}
---
+++ StackOverflowClone.ServiceLayer/QuestionsService.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StackOverflowClone.DomainModels;
+using StackOverflowClone.ViewModels;
+using StackOverflowClone.Repositories;
+
+using AutoMapper;
+using AutoMapper.Configuration;
+
+
+namespace StackOverflowClone.ServiceLayer
+{
+    public interface IQuestionsService
+    {
+        void InsertQuestion(NewQuestionModel questionViewModel);
+        void UpdateQuestionDetails(EditQuestionViewModel questionViewModel);
+        void UpdateQuestionVotesCount(int questionID, int value);
+        void UpdateQuestionAnswerCount(int questionID, int value);
+        void UpdateQuestionViewCount(int questionID, int value);
+        void DeleteQuestion(int questionID);
+        List<QuestionViewModel> GetAllQuestions();
+        QuestionViewModel GetQuestionByQuestionID(int questionID, int UserID);
+    }
+
+
+    public class QuestionsService : IQuestionsService
+    {
+        IQuestionsRepository qr;
+
+        public QuestionsService()
+        {
+            qr = new QuestionsRepository();
+
+        }
+
+        public void DeleteQuestion(int questionID)
+        {
+            qr.DeleteQuestion(questionID);
+        }
+
+        public List<QuestionViewModel> GetAllQuestions()
+        {
+            List<Question> questionRep = qr.GetAllQuestions();
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Question, QuestionViewModel>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            List<QuestionViewModel> questions = mapper.Map<List<Question>, List<QuestionViewModel>>(questionRep);
+            return questions;
+        }
+
+        public QuestionViewModel GetQuestionByQuestionID(int questionID, int UserID=0)
+        {
+            Question questionRep = qr.GetQuestionByQuestionID(questionID).FirstOrDefault();
+            QuestionViewModel questionSer = null;
+            if (questionRep == null)
+            {
+                var config = new MapperConfiguration(cfg => { cfg.CreateMap<Question, QuestionViewModel>(); cfg.IgnoreUnmapped(); });
+                IMapper mapper = config.CreateMapper();
+
+                questionSer = mapper.Map<Question, QuestionViewModel>(questionRep);
+            }
+
+
+
+            return questionSer;
+
+
+        }
+
+        public void InsertQuestion(NewQuestionModel questionViewModel)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<NewQuestionModel, Question>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Question question = mapper.Map<NewQuestionModel, Question>(questionViewModel);
+            qr.InsertQuestion(question);
+        }
+
+        public void UpdateQuestionAnswerCount(int questionID, int value)
+        {
+            qr.UpdateQuestionAnswerCount(questionID, value);
+        }
+
+        public void UpdateQuestionDetails(EditQuestionViewModel questionViewModel)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<EditQuestionViewModel, Question>(); cfg.IgnoreUnmapped(); });
+
+            IMapper mapper = config.CreateMapper();
+            Question question = mapper.Map<EditQuestionViewModel, Question>(questionViewModel);
+            qr.UpdateQuestionDetails(question);
+        }
+
+        public void UpdateQuestionViewCount(int questionID, int value)
+        {
+            qr.UpdateQuestionViewCount(questionID, value);
+        }
+
+        public void UpdateQuestionVotesCount(int questionID, int value)
+        {
+            qr.UpdateQuestionVotesCount(questionID, value);
+
+        }
+    }
+}
